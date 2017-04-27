3582e85e3659dfa6eec5c1e50d41f9410ce0e22b
techAmi <allali@techfirm.co.jp>
2017-04-27 13:57:46 +0900

    created environment_app.component.ts
    this is just a tutorial

---
+++ environment_app.component.ts
@@ -0,0 +1,16 @@
+
+
+import {Component, View} from "angular2/core"; 
+
+@Component({
+    selector: 'my-app'
+})
+
+@View({
+    template: "<h2> My first Angular 2 app </h2>"
+})
+
+export class AppComponent{
+
+}
+
