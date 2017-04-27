70984019eb2d44967301483bd875b722e3891ebd
Allali Amina <allali@in.techfirm.co.jp>
2017-04-27 14:11:28 +0900

    I added some comment to environment_app_component

--- environment_app.component.ts
+++ environment_app.component.ts
@@ -1,4 +1,17 @@
-
+/**this code will import the Component and view package 
+ * from angular2/core
+ * 
+ * the @component is an angular 2 decorator that allows 
+ * to associate metadata with the component
+ * class
+ * the my-app can be used as an HTML tag to injecting and 
+ * can be used as a component
+ * 
+ * the @view contains a template that tells angular how to 
+ * render a view
+ * 
+ * the export specifies that, this
+ */
 
 import {Component, View} from "angular2/core"; 
 
